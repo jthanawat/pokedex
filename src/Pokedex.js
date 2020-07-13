fa9726e3307e209cc782a60272e7e3c33783230f
jthanawat <kla13thanawat@gmail.com>
2020-07-14 01:28:49 +0700

    set up search bar

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { AppBar, Toolbar, Grid, Card, CardMedia, CardContent, CircularProgress, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Grid, Card, CardMedia, CardContent, CircularProgress, Typography, TextField } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import { fade, makeStyles } from '@material-ui/core/styles'
 import mockData from './mockData'
 import { toFirstCharUpperCase } from './constants'
 import axios from 'axios';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   pokedexContainer: {
     paddingTop: '20px',
     paddingLeft: '50px',
@@ -16,8 +17,24 @@ const useStyles = makeStyles({
   },
   cardContent: {
     textAlign: 'center'
-  }
-})
+  },
+  searchContainer: {
+    display: 'flex',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    marginTop: '5px',
+    marginBottom: '5px',
+  },
+  searchIcon: {
+    alignSelf: "flex-end",
+    marginBottom: '5px',
+  },
+  searchInput: {
+    width: '200px',
+    margin: '5px'
+  },
+}))
 
 
 const Pokedex = (props) => {
@@ -25,6 +42,11 @@ const Pokedex = (props) => {
   console.log(history)
   const classes = useStyles();
   const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  }
 
   useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
@@ -35,7 +57,7 @@ const Pokedex = (props) => {
         const newPokemonData = {};
         results.forEach((pokemon, index) => {
           newPokemonData[index + 1] = {
-            id: index +  1,
+            id: index + 1,
             name: pokemon.name,
             sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
           };
@@ -69,11 +91,22 @@ const Pokedex = (props) => {
   return (
     <>
       <AppBar position="static">
-        <Toolbar />
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField
+              onChange={handleSearchChange}
+              className={classes.searchInput}
+              label="Pokemon"
+              variant="standard"
+            />
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
           {Object.keys(pokemonData).map(pokemonId =>
+          pokemonData[pokemonId].name.includes(filter) &&
             getPokemonCard(pokemonId)
           )}
         </Grid>
